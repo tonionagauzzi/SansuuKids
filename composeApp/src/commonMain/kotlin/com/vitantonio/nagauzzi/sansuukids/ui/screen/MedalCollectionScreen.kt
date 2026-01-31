e6ff89a801d59168238fab77a6073360a5fd3c6e
tonionagauzzi <vitantonionagauzzi@gmail.com>
2026-01-31 19:23:56 +0900

    直前のコミットでメダル図鑑のスクロールが消えていたので復活

--- composeApp/src/commonMain/kotlin/com/vitantonio/nagauzzi/sansuukids/ui/screen/MedalCollectionScreen.kt
+++ composeApp/src/commonMain/kotlin/com/vitantonio/nagauzzi/sansuukids/ui/screen/MedalCollectionScreen.kt
@@ -3,7 +3,6 @@ package com.vitantonio.nagauzzi.sansuukids.ui.screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -55,19 +56,13 @@ internal fun MedalCollectionScreen(
                 modifier = Modifier.testTag("medal_collection_title")
             )
 
-            Box(
+            MedalGrid(
+                medalDisplays = medalDisplays,
                 modifier = Modifier
-                    .padding(top = 16.dp)
-                    .weight(1f),
-                contentAlignment = Alignment.Center
-            ) {
-                MedalGrid(
-                    medalDisplays = medalDisplays,
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .testTag("medal_grid")
-                )
-            }
+                    .padding(16.dp)
+                    .testTag("medal_grid")
+                    .verticalScroll(rememberScrollState())
+            )
         }
     }
 }
